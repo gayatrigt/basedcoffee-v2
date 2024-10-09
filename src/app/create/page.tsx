b9659a247e78593f4d3cc2d4c28117b3c043ae7a
Utkarsh Bhimte <workwithutkarsh@gmail.com>
2024-10-09 11:05:32 +0530

    Refactor CreatePage component to use dynamic height and update dependencies

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import { Combobox, ComboboxButton, ComboboxInput, ComboboxOption, ComboboxOptions } from '@headlessui/react';
 import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid';
+import { twMerge } from 'tailwind-merge';
 
 const categories = [
     { id: 1, name: 'Education' },
@@ -15,7 +16,7 @@ const categories = [
 const CreatePage: React.FC = () => {
     const [query, setQuery] = useState('');
     console.log("🚀 ~ query:", query)
-    const [selectedCategory, setSelectedCategory] = useState(null);
+    const [selectedCategory, setSelectedCategory] = useState<number>(1);
 
     const filteredCategories =
         !query.length
@@ -40,53 +41,23 @@ const CreatePage: React.FC = () => {
             </div>
 
             <div className="w-full max-w-md space-y-4">
-                <Combobox value={selectedCategory} onChange={setSelectedCategory}>
-                    <div className="relative mt-1">
-                        <ComboboxInput
-                            className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white"
-                            onChange={(event) => setQuery(event.target.value)}
-                            displayValue={(category: { name: string } | null) => category?.name || ''}
-                            placeholder="Select a category"
-                        />
-                        <ComboboxButton className="absolute inset-y-0 right-0 flex items-center pr-2">
-                            <ChevronUpDownIcon
-                                className="h-5 w-5 text-gray-400"
-                                aria-hidden="true"
-                            />
-                        </ComboboxButton>
-                        <ComboboxOptions className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                            {filteredCategories.map((category) => (
-                                <ComboboxOption
-                                    key={category.id}
-                                    value={category}
-                                    className={({ active }) =>
-                                        `relative cursor-default select-none py-2 pl-10 pr-4 ${active ? 'bg-blue-600 text-white' : 'text-gray-900'
-                                        }`
-                                    }
-                                >
-                                    {({ selected, active }) => (
-                                        <>
-                                            <span
-                                                className={`block truncate ${selected ? 'font-medium' : 'font-normal'
-                                                    }`}
-                                            >
-                                                {category.name}
-                                            </span>
-                                            {selected ? (
-                                                <span
-                                                    className={`absolute inset-y-0 left-0 flex items-center pl-3 ${active ? 'text-white' : 'text-blue-600'
-                                                        }`}
-                                                >
-                                                    <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                                                </span>
-                                            ) : null}
-                                        </>
-                                    )}
-                                </ComboboxOption>
-                            ))}
-                        </ComboboxOptions>
+
+
+                <div>
+                    <h2 className="text-lg text-gray-600 mb-1">Category</h2>
+                    <div className="flex flex-wrap gap-2">
+                        {categories.map((category) => <span
+                            className={twMerge(
+                                'py-1 px-2 border-2 rounded-lg text-sm',
+                                selectedCategory === category.id ? 'border-blue-600 text-blue-600' : 'border-gray-300 text-gray-600'
+                            )}
+                            onClick={() => setSelectedCategory(category.id)}
+                            key={category.id}
+                        >
+                            {category.name}
+                        </span>)}
                     </div>
-                </Combobox>
+                </div>
 
                 <div>
                     <h2 className="text-lg text-gray-600">What do you want to raise funds for?</h2>
@@ -117,7 +88,8 @@ const CreatePage: React.FC = () => {
 
                 <button
                     type="submit"
-                    className="w-full bg-blue-600 text-white font-semibold py-2 px-4 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 sticky bottom-2"
+                    // className="w-full bg-blue-600 text-white font-semibold py-2 px-4 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 sticky bottom-2"
+                    className='bg-blue-600 text-white/80 py-2 w-full mt-2 rounded-md font-semibold'
                 >
                     Create Fundraise
                 </button>
