5eaba9a224224af5caca3ed73472acded2352808
Utkarsh Bhimte <workwithutkarsh@gmail.com>
2024-10-16 22:45:56 +0530

    Disable unsafe TypeScript rules and update ESLint configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,19 +15,25 @@ const config = {
   "rules": {
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
-    "@typescript-eslint/consistent-type-imports": [
-      "warn",
-      {
-        "prefer": "type-imports",
-        "fixStyle": "inline-type-imports"
-      }
-    ],
-    "@typescript-eslint/no-unused-vars": [
-      "warn",
-      {
-        "argsIgnorePattern": "^_"
-      }
-    ],
+
+
+    // Disable the unsafe argument rule
+    "@typescript-eslint/no-unsafe-argument": "off",
+
+    // Disable the unsafe assignment rule
+    "@typescript-eslint/no-unsafe-assignment": "off",
+
+    "@typescript-eslint/consistent-type-imports": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/prefer-nullish-coalescing": "off",
+    "@typescript-eslint/prefer-optional-chain": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-unsafe-call": "off",
+    "@typescript-eslint/restrict-template-expressions": "off",
+    "@typescript-eslint/no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
+    "@typescript-eslint/no-unnecessary-type-assertion": "off",
+    "react-hooks/exhaustive-deps": "warn",
+
     "@typescript-eslint/require-await": "off",
     "@typescript-eslint/no-misused-promises": [
       "error",
